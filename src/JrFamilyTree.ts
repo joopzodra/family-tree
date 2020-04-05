2681093f000a42152ab80c4143c65799960d92bd
Joop Rosier <joop@zodra.nl>
2020-04-05 15:26:55 +0200

    parent connectors

--- src/JrFamilyTree.ts
+++ src/JrFamilyTree.ts
@@ -1,6 +1,9 @@
 import { LitElement, html, css, property, TemplateResult, CSSResult } from 'lit-element'
 import { Person } from './models/person.model'
 
+const connectWidth = 2
+const connectColor = css`rgba(0,0,0, 0.4)`
+
 export class JrFamilyTree extends LitElement {
   @property() data: Array = []
   @property() mainId: Number = 28
@@ -25,13 +28,35 @@ export class JrFamilyTree extends LitElement {
         align-items: center;
         justify-content: flex-end;
       }
-      .parent {
+      .person {
         display: flex;
         flex-flow: column;
         width: 100px;
         height: 100px;
-        border: solid 2px red;
-        margin: 16px;
+        border: solid ${connectColor} 2px;
+        margin: 0 16px;
+      }
+      .ancestor-vertical-connect {
+        width: ${connectWidth}px;
+        height: 16px;
+        background-color: ${connectColor};
+        opacity: 0.5;
+      }
+      .ancestor-horizontal-connect {
+        height: ${connectWidth}px;
+        background-color:  ${connectColor};
+        opacity: 0.5;
+        width: 50%;
+      }
+      .ancestor-horizontal-connect.left {
+        margin-left: 50%;
+        padding-left: ${connectWidth / 2}px;
+        margin-right: ${connectWidth / 2}px;
+      }
+      .ancestor-horizontal-connect.right {
+        margin-right: 50%;
+        padding-right: ${connectWidth / 2}px;
+        margin-left: ${connectWidth / 2}px;
       }
     `;
   }
@@ -53,23 +78,32 @@ export class JrFamilyTree extends LitElement {
     let mother
     if (person.fatherId) {
       const fatherData = data.find(p => p.id === person.fatherId) as Person
-      father = JrFamilyTree.getAncestorTemplate(data, fatherData);
+      father = JrFamilyTree.getAncestorTemplate(data, fatherData, 'left');
     }
     if (person.motherId) {
       const motherData = data.find(p => p.id === person.motherId) as Person
-      mother = JrFamilyTree.getAncestorTemplate(data, motherData)
+      mother = JrFamilyTree.getAncestorTemplate(data, motherData, 'right')
     }
+    // mother = !mother && father ? html`<div>ONBEKEND</div>` : mother
+    // father = !father && mother ? html`<div>ONBEKEND</div>` : father
     return html`${father}${mother}`
   }
 
-  static getAncestorTemplate(data: Person[], person: Person): TemplateResult {
+  static getAncestorTemplate(data: Person[], person: Person, leftOrRight: 'left' | 'right'): TemplateResult {
+    const ancestorData = JrFamilyTree.getAncestors(data, person)
+    const ancestorVerticalConnectTop = ancestorData.values.includes(undefined) ?
+      html`` : html`<div class="ancestor-vertical-connect"></div>`
     return html`
-        <div class="ancestors">
-          <div class="ancestors-container">${JrFamilyTree.getAncestors(data, person)}</div>  
-          <a class="parent">
-            <div>${person.firstNames} ${person.surname}</div><div>${person.dateOfBirth}</div>
-          </a>
-      </div>`
+      <div class="ancestors">
+        <div class="ancestors-container">${ancestorData}</div>
+        ${ancestorVerticalConnectTop}
+        <a class="person">
+          <div>${person.firstNames} ${person.surname}</div><div>${person.dateOfBirth}</div>
+        </a>
+        <div class="ancestor-vertical-connect"></div>
+        <div class="ancestor-horizontal-connect ${leftOrRight}"></div>
+      </div>
+      `
   }
 
   static getProgeny(data: Person[], person: Person): Person {
